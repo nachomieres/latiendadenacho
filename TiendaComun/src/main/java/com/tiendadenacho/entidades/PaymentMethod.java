8d222d4d46fccd2c7eba3030625eb4e0401b5362
Nacho Gonzalez <nachomieres@gmail.com>
2022-02-01 21:40:06 +0100

    gestion de los pedidos en backend

--- TiendaComun/src/main/java/com/tiendadenacho/entidades/PaymentMethod.java
+++ TiendaComun/src/main/java/com/tiendadenacho/entidades/PaymentMethod.java
@@ -1,5 +1,5 @@
 package com.tiendadenacho.entidades;
 
 public enum PaymentMethod {
-	COD, CREDIT_CARD
+	COD, TARJETA_CREDITO
 }
---
+++ TiendaWebBase/TiendaBackEnd/src/main/java/com/tiendadenacho/admin/order/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tiendadenacho.admin.order;
+
+public class OrderNotFoundException extends Exception {
+
+	public OrderNotFoundException(String message) {
+		super(message);
+	}
+
+}
