21684531447d26b09d1176bdad4990887b559325
Nacho Gonzalez <nachomieres@gmail.com>
2022-02-13 21:13:42 +0100

    Fallo en el Procfile

---
+++ TiendaWebBase/TiendaBackEnd/target/classes/static/js/common_list.js
@@ -0,0 +1,11 @@
+function clearFilter() {
+	window.location = moduleURL;	
+}
+
+function showDeleteConfirmModal(link, entityName) {
+	entityId = link.attr("entityId");
+	
+	$("#yesButton").attr("href", link.attr("href"));	
+	$("#confirmText").text("¿Borrar " + entityName + " con ID " + entityId + "?");
+	$("#confirmModal").modal();	
+}
---
+++ TiendaWebBase/TiendaBackEnd/target/classes/static/js/states_settings.js
@@ -0,0 +1,202 @@
+var buttonLoad4States;
+var dropDownCountry4States;
+var dropDownStates;
+var buttonAddState;
+var buttonUpdateState;
+var buttonDeleteState;
+var labelStateName;
+var fieldStateName;
+
+$(document).ready(function() {
+	buttonLoad4States = $("#buttonLoadCountriesForStates");
+	dropDownCountry4States = $("#dropDownCountriesForStates");
+	dropDownStates = $("#dropDownStates");
+	buttonAddState = $("#buttonAddState");
+	buttonUpdateState = $("#buttonUpdateState");
+	buttonDeleteState = $("#buttonDeleteState");
+	labelStateName = $("#labelStateName");
+	fieldStateName = $("#fieldStateName");
+	
+	buttonLoad4States.click(function() {
+		loadCountries4States();
+	});
+	
+	dropDownCountry4States.on("change", function() {
+		loadStates4Country();
+	});
+
+	dropDownStates.on("change", function() {
+		changeFormStateToSelectedState();
+	});
+		
+	buttonAddState.click(function() {
+		if (buttonAddState.val() == "Añadir") {
+			addState();
+		} else {
+			changeFormStateToNew();
+		}
+	});
+	
+	buttonUpdateState.click(function() {
+		updateState();
+	});
+	
+	buttonDeleteState.click(function() {
+		deleteState();
+	});
+});
+
+function deleteState() {
+	stateId = dropDownStates.val();
+	
+	url = contextPath + "states/delete/" + stateId;
+	
+	$.ajax({
+		type: 'DELETE',
+		url: url,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		}
+	}).done(function() {
+		$("#dropDownStates option[value='" + stateId + "']").remove();
+		changeFormStateToNew();
+		showToastMessage("La Provincia se ha borrado");
+	}).fail(function() {
+		showToastMessage("ERROR: no se puede conectar con el servidor");
+	});			
+}
+
+function updateState() {
+	url = contextPath + "states/save";
+	stateId = dropDownStates.val();
+	stateName = fieldStateName.val();
+	
+	selectedCountry = $("#dropDownCountriesForStates option:selected");
+	countryId = selectedCountry.val();
+	countryName = selectedCountry.text();
+	
+	jsonData = {id: stateId, name: stateName, country: {id: countryId, name: countryName}};
+	
+	$.ajax({
+		type: 'POST',
+		url: url,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		},
+		data: JSON.stringify(jsonData),
+		contentType: 'application/json'
+	}).done(function(stateId) {
+		$("#dropDownStates option:selected").text(stateName);
+		showToastMessage("The state has been updated");
+		changeFormStateToNew();
+	}).fail(function() {
+		showToastMessage("ERROR: no se puede conectar con el servidor");
+	});	
+}
+
+function validateFormState() {
+	formState = document.getElementById("formState");
+	if (!formState.checkValidity()) {
+		formState.reportValidity();
+		return false;
+	}	
+	
+	return true;
+}
+
+function addState() {
+	if (!validateFormState()) return;
+	
+	url = contextPath + "states/save";
+	stateName = fieldStateName.val();
+	
+	selectedCountry = $("#dropDownCountriesForStates option:selected");
+	countryId = selectedCountry.val();
+	countryName = selectedCountry.text();
+	
+	jsonData = {name: stateName, country: {id: countryId, name: countryName}};
+	
+	$.ajax({
+		type: 'POST',
+		url: url,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		},
+		data: JSON.stringify(jsonData),
+		contentType: 'application/json'
+	}).done(function(stateId) {
+		selectNewlyAddedState(stateId, stateName);
+		showToastMessage("La Provincia se ha añadido");
+	}).fail(function() {
+		showToastMessage("ERROR: no se puede conectar con el servidor");
+	});
+		
+}
+
+function selectNewlyAddedState(stateId, stateName) {
+	$("<option>").val(stateId).text(stateName).appendTo(dropDownStates);
+	
+	$("#dropDownStates option[value='" + stateId + "']").prop("selected", true);
+	
+	fieldStateName.val("").focus();
+}
+
+function changeFormStateToNew() {
+	buttonAddState.val("Añadir");
+	labelStateName.text("Provincia:");
+	
+	buttonUpdateState.prop("disabled", true);
+	buttonDeleteState.prop("disabled", true);
+	
+	fieldStateName.val("").focus();	
+}
+
+function changeFormStateToSelectedState() {
+	buttonAddState.prop("value", "Añadir");
+	buttonUpdateState.prop("disabled", false);
+	buttonDeleteState.prop("disabled", false);
+	
+	labelStateName.text("Provincia seleccionado:");
+	
+	selectedStateName = $("#dropDownStates option:selected").text();
+	fieldStateName.val(selectedStateName);
+	
+}
+
+function loadStates4Country() {
+	selectedCountry = $("#dropDownCountriesForStates option:selected");
+	countryId = selectedCountry.val();
+	url = contextPath + "states/list_by_country/" + countryId;
+	
+	$.get(url, function(responseJSON) {
+		dropDownStates.empty();
+		
+		$.each(responseJSON, function(index, state) {
+			$("<option>").val(state.id).text(state.name).appendTo(dropDownStates);
+		});
+		
+	}).done(function() {
+		changeFormStateToNew();
+		showToastMessage("Todas las Provincias cargados para " + selectedCountry.text());
+	}).fail(function() {
+		showToastMessage("ERROR: no se puede conectar con el servidor");
+	});	
+}
+
+function loadCountries4States() {
+	url = contextPath + "countries/list";
+	$.get(url, function(responseJSON) {
+		dropDownCountry4States.empty();
+		
+		$.each(responseJSON, function(index, country) {
+			$("<option>").val(country.id).text(country.name).appendTo(dropDownCountry4States);
+		});
+		
+	}).done(function() {
+		buttonLoad4States.val("Actualizar lista de paises");
+		showToastMessage("Lista de paises cargada");
+		loadStates4Country();
+	}).fail(function() {
+		showToastMessage("ERROR: no se puede conectar con el servidor");
+	});
+}
